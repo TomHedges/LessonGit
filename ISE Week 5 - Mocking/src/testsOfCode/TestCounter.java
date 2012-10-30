7699c12195227abdc1c6d6684ebcf12969488c06
thedge01 <thedge01@bbk.ac.uk>
2012-10-30 19:15:26 +0000

    Added some Mock tests

--- ISE Week 5 - Mocking/src/testsOfCode/TestCounter.java
+++ ISE Week 5 - Mocking/src/testsOfCode/TestCounter.java
@@ -18,10 +18,47 @@ public class TestCounter {
 		Integer first;
 		Integer expectedFirst=1;
 
+		Counter countTestA = new Counter();
+
+		first = countTestA.getValue();
+
+		assertEquals("Wrong Answer from non-mock test!",expectedFirst, first);
+	}
+
+	@Test
+	public void testAMock() {
+		Integer first, mockedFirst;
+		//Integer expectedFirst = 1;
+
+		Counter countTestAMock = new Counter();
+
+		Counter mockedCounter = mock(Counter.class);
+		when(mockedCounter.getValue()).thenReturn(1);
+
+		first = countTestAMock.getValue();
+		mockedFirst= mockedCounter.getValue();
+
+		assertEquals("Wrong Answer from MOCK!",first , mockedFirst);
+	}
+
+	@Test
+	public void testBMock() {
+		Integer first,mockedFirst;
+		//Integer expectedFirst=1;
+		
 		Counter count = new Counter();
-
+		
+		Counter mockedCounter= mock(Counter.class);
+		when(mockedCounter.getValue()).thenReturn(1).thenReturn(2);
+		
 		first = count.getValue();
-
-		assertEquals("Wrong Answer!",expectedFirst, first);
+		mockedFirst= mockedCounter.getValue();
+		
+		assertEquals("Wrong Answer from MOCK test B - run 1 !",first , mockedFirst);
+		
+		first = count.getValue();
+		mockedFirst= mockedCounter.getValue();
+		
+		assertEquals("Wrong Answer from MOCK test B - run 2 ",first , mockedFirst);
 	}
 }
